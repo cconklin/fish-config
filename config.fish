af20819d99c13b3b59ad277e3535fbe10cf0d478
Chase Conklin <chase@conklins.net>
2016-02-27 18:05:20 -0500

    updated config.fish

--- config.fish
+++ config.fish
@@ -11,10 +11,21 @@ function fish_prompt
   else
     set __prompt_pwd (basename (pwd))
   end
+  if set -q VIRTUAL_ENV
+    set __virtualenv (printf '[%s]' (basename "$VIRTUAL_ENV"))
+  else
+    set __virtualenv ""
+  end
   if [ (__fish_git_prompt) ]
     printf 'MacBook Pro:%s%s %s $ ' $__prompt_pwd (__fish_git_prompt | tr -d '[[:space:]]') $LOGNAME  
   else
     printf 'MacBook Pro:%s %s $ ' $__prompt_pwd $LOGNAME
+  end
+end
+function fish_right_prompt
+  if set -q VIRTUAL_ENV
+    set __virtualenv (echo -n -s (set_color blue) (basename "$VIRTUAL_ENV") (set_color normal))
+    printf '(%s)' $__virtualenv
   end
 end
 function diff
@@ -23,4 +34,10 @@ end
 function less
   command less -F -R $argv
 end
+function nosetests
+  command nosetests --rednose $argv
+end
+eval (python -m virtualfish auto_activation)
+set PATH ~/bin/ $PATH
 rvm default
+
